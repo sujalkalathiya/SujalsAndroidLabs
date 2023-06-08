134f3dc2a0a700f29e4b4cb498f25baa290e95fc
Sujal Kalathiya <kala0049@algonquinlive.com>
2023-06-07 20:19:48 -0400

    Finished week 2

--- app/src/main/java/algonquin/cst2335/kala0049/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/kala0049/ui/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import algonquin.cst2335.kala0049.data.MainViewModel;
 import algonquin.cst2335.kala0049.databinding.ActivityMainBinding;
@@ -26,6 +27,29 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String editString = variableBinding.editText.getText().toString();
                 variableBinding.mytextview.setText( "Your edit text has: " + editString);
+            }
+        });
+        variableBinding.checkBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            model.isSelected.postValue(isChecked);
+        });
+        variableBinding.switch1.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            model.isSelected.postValue(isChecked);
+        });
+        variableBinding.radioButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            model.isSelected.postValue(isChecked);
+        });
+        model.isSelected.observe(this, selected -> {
+            variableBinding.checkBox.setChecked(selected);
+            variableBinding.switch1.setChecked(selected);
+            variableBinding.radioButton.setChecked(selected);
+            Toast.makeText(getApplicationContext(),"the value is now: "+ selected, Toast.LENGTH_SHORT).show();
+        });
+        variableBinding.myimageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int width = variableBinding.imageView.getWidth();
+                int height = variableBinding.imageView.getHeight();
+                Toast.makeText(getApplicationContext(), "The width = " + width + " and height = " + height,Toast.LENGTH_SHORT).show();
             }
         });
     }
